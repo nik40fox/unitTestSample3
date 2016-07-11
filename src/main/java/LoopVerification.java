53245df3ede6560ec6e8faec5e4c702509723f64
nik40fox <nedubrovin@gmail.com>
2016-07-11 17:44:47 +0300

    ready to write test importet junit

--- src/main/java/LoopVerification.java
+++ src/main/java/LoopVerification.java
@@ -3,8 +3,8 @@ public class LoopVerification {
     public static void main(String args[]){
         int y = 1;
         int k;
-        for(k = 6; k >= 3; k--)
-            y = y + k;
+        for(k = 6; k >= 3; k--){
+            y = y + k;}
         System.out.println("y = " + y);
         System.out.println("k = " + k);
     }
---
+++ src/test/java/LoopVerificationTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Nikolay on 11.07.2016.
+ */
+import junit.framework.TestCase;
+import org.junit.*;
+
+public class LoopVerificationTest extends TestCase {
+    @Test
+    public void runTest(){
+
+    }
+}
