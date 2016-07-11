c47d13e1d649c0cb5147a8477eb572f032219391
nik40fox <nedubrovin@gmail.com>
2016-07-11 17:23:46 +0300

    added .gitignore , . exclude ask teacher about .exclude
    added all files in project

---
+++ src/main/java/LoopVerification.java
@@ -0,0 +1,11 @@
+
+public class LoopVerification {
+    public static void main(String args[]){
+        int y = 1;
+        int k;
+        for(k = 6; k >= 3; k--)
+            y = y + k;
+        System.out.println("y = " + y);
+        System.out.println("k = " + k);
+    }
+}
